81cb9072ba4d0d4ec0fbb682b3288cdb3cac54f3
abir14016 <abir14016@gmail.com>
2023-08-23 22:13:53 +0600

    explore dynamic data fetching

---
+++ src/app/product/[id]/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='flex justify-center items-center h-[100vh]'>
+            <h2 className='text-4xl'>Loading...</h2>
+        </div>
+    );
+};
+
+export default loading;
